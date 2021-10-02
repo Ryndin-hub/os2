1af9abc1c780a2d3e38f00e35a75d1600d3d3237
Ryndin-hub <72243480+Ryndin-hub@users.noreply.github.com>
2021-10-02 09:45:46 +0700

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define PTHREAD_CREATE_SUCCESS 0
+#define EXIT_ERROR 1
+#define EXIT_SUCCESS 0
+
+void *print_line(void *arg) {
+    int* a = (int*) arg;
+    printf("thread %d\n",*a);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int a[4] = {1,2,3,4};
+    for (int i = 0; i < 4; i++){
+        int pthread_create_result = pthread_create(&thread, NULL, print_line, (void*) &a[i]);
+        if (pthread_create_result != PTHREAD_CREATE_SUCCESS) {
+            perror("Unable to create thread");
+            exit(EXIT_ERROR);
+        }
+    }
+    pthread_exit(NULL);
+    return EXIT_SUCCESS;
+}
