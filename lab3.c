176433b18a6ce74d4de62057b10374560be790ae
Ryndin-hub <72243480+Ryndin-hub@users.noreply.github.com>
2021-10-09 10:09:44 +0700

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -12,14 +12,17 @@ void *print_line(void *arg) {
 }
 
 int main() {
-    pthread_t thread;
+    pthread_t thread[4];
     int a[4] = {1,2,3,4};
     for (int i = 0; i < 4; i++){
-        int pthread_create_result = pthread_create(&thread, NULL, print_line, (void*) &a[i]);
+        int pthread_create_result = pthread_create(&thread[i], NULL, print_line, (void*) &a[i]);
         if (pthread_create_result != PTHREAD_CREATE_SUCCESS) {
             perror("Unable to create thread");
             exit(EXIT_ERROR);
         }
+    }
+    for (int i = 0; i < 4; i++){
+        pthread_join(thread[i], NULL);
     }
     pthread_exit(NULL);
     return EXIT_SUCCESS;
